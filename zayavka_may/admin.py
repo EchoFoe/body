101715f30d6d4fd267023f6186be5ac1e48ceed3
EchoFoe <Aspirpd@gmail.com>
2019-05-12 12:53:34 +0300

    version_196

--- zayavka_may/admin.py
+++ zayavka_may/admin.py
@@ -3,9 +3,10 @@ from .models import *
 
 
 class Zayavka_mayAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Zayavka_may._meta.fields]
-    list_filter = ['email', 'last_name']
-    search_fields = ['email', 'last_name']
+    # list_display = [field.name for field in Zayavka_may._meta.fields]
+    list_display = ['last_name', 'first_name', 'gender', 'age', 'birthday', 'sostav', 'message', 'weight', 'raised_weight', 'wilkes', 'created']
+    list_filter = ['last_name', 'sostav', 'message']
+    search_fields = ['last_name', 'first_name', 'message']
 
     class Meta:
         model = Zayavka_may

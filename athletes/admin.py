f31eddbb8e0b275ff9d29152bacd4daa8ab90d56
EchoFoe <Aspirpd@gmail.com>
2019-05-18 15:50:23 +0300

    version_205

--- athletes/admin.py
+++ athletes/admin.py
@@ -83,11 +83,12 @@ class AthletesImageInline(admin.TabularInline):
 
     admin.site.register(Line_up, Line_upAdmin)
 
+
 class AthletesAdmin(admin.ModelAdmin):
     filter_horizontal = ('discipline', 'age_category')
     inlines = [AthletesImageInline]
     list_display = ['last_name', 'first_name', 'gender', 'age', 'Возрастные_категории', 'weight', 'weight_category', 'Дивизион', 'Дисциплины', 'Турнир', 'wilkes', 'created']
-    list_filter = ['division', 'discipline', 'gender', 'age_category', 'weight_category', 'tournament', 'line_up', 'town']
+    list_filter = ['division', 'discipline', 'gender', 'age_category', 'weight_category', 'tournament', 'status', 'line_up', 'town']
     search_fields = ['first_name', 'last_name']
 
     class Meta:

2863e5bf3c562d85e7bc4a7dc71e81a493f747cb
EchoFoe <Aspirpd@gmail.com>
2019-06-23 23:48:17 +0300

    hate

--- athletes/admin.py
+++ athletes/admin.py
@@ -86,7 +86,7 @@ class AthletesImageInline(admin.TabularInline):
 
 class AthletesAdmin(admin.ModelAdmin):
     save_as = True
-    filter_horizontal = ('discipline', 'age_category')
+    filter_horizontal = ['age_category']
     inlines = [AthletesImageInline]
     list_display = ['last_name', 'first_name', 'gender', 'age', 'Возрастные_категории', 'weight', 'weight_category', 'Дивизион', 'Турнир', 'wilkes', 'created']
     list_filter = ['division', 'discipline', 'gender', 'age_category', 'weight_category', 'tournament', 'status', 'line_up', 'town']
--- athletes/models.py
+++ athletes/models.py
@@ -106,7 +106,7 @@ class Athletes(models.Model):
     trainer = models.CharField(max_length=64, blank=True, verbose_name='Тренер')
     tournament = models.ForeignKey(Tournaments, on_delete=models.CASCADE, verbose_name='Турнир')
     division = models.ForeignKey(Division, on_delete=models.CASCADE, verbose_name='Дивизион')
-    discipline = models.ManyToManyField(Discipline, blank=True, default=True, verbose_name='Дисциплины')
+    discipline = models.ForeignKey(Discipline, blank=True, null=True, on_delete=models.CASCADE, default=True, verbose_name='Дисциплины')
     message = models.TextField(max_length=128, blank=True, null=True, default=None, verbose_name='Сообщение')
     status = models.ForeignKey(Status, on_delete=models.CASCADE, default=id(1), verbose_name='Статус')
     is_active = models.BooleanField(default=True, verbose_name='Актуальность')
@@ -125,9 +125,9 @@ class Athletes(models.Model):
     def Дивизион(self):
         return self.division
 
-    @property
-    def Дисциплины(self):
-        return "\n".join([p.name for p in self.discipline.all()])
+    # @property
+    # def Дисциплины(self):
+    #     return "\n".join([p.name for p in self.discipline.all()])
 
     @property
     def Возрастные_категории(self):
